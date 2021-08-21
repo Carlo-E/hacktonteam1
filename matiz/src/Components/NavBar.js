4611aec5c10182311a37d139ef165ca83bcebcb4
Yashiry Azize <azizeyashiry@gmail.com>
2021-08-21 13:09:01 -0400

    fix names

---
+++ matiz/src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+
+const NavBar = () => {
+    return (
+        <section>
+            <h2>Matiz</h2>
+            <Link>chicken</Link>
+        </section>
+    )
+}
+
+
+export default NavBar
---
+++ matiz/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Home  = () => {
+    return (
+        <div>
+            <h4></h4>
+            <button><Link></Link></button>
+        </div>
+    )
+}
+
+export default Home 
